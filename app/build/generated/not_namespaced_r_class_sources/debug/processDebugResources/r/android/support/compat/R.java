86f49db8947d67bfed45b7444c6417320b1acba9
Lavrenty87 <Lavr1987114@yandex.ru>
2019-05-07 16:17:51 +0300

    A1HW4

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/compat/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/compat/R.java
@@ -99,13 +99,13 @@ public final class R {
         public static final int notification_main_column_container = 0x7f070056;
         public static final int right_icon = 0x7f07005f;
         public static final int right_side = 0x7f070060;
-        public static final int tag_transition_group = 0x7f070080;
-        public static final int tag_unhandled_key_event_manager = 0x7f070081;
-        public static final int tag_unhandled_key_listeners = 0x7f070082;
-        public static final int text = 0x7f070083;
-        public static final int text2 = 0x7f070084;
-        public static final int time = 0x7f070087;
-        public static final int title = 0x7f070088;
+        public static final int tag_transition_group = 0x7f070086;
+        public static final int tag_unhandled_key_event_manager = 0x7f070087;
+        public static final int tag_unhandled_key_listeners = 0x7f070088;
+        public static final int text = 0x7f070089;
+        public static final int text2 = 0x7f07008a;
+        public static final int time = 0x7f07008d;
+        public static final int title = 0x7f07008e;
     }
     public static final class integer {
         private integer() {}
@@ -115,17 +115,17 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f09001d;
-        public static final int notification_action_tombstone = 0x7f09001e;
-        public static final int notification_template_custom_big = 0x7f09001f;
-        public static final int notification_template_icon_group = 0x7f090020;
-        public static final int notification_template_part_chronometer = 0x7f090021;
-        public static final int notification_template_part_time = 0x7f090022;
+        public static final int notification_action = 0x7f09001e;
+        public static final int notification_action_tombstone = 0x7f09001f;
+        public static final int notification_template_custom_big = 0x7f090020;
+        public static final int notification_template_icon_group = 0x7f090021;
+        public static final int notification_template_part_chronometer = 0x7f090022;
+        public static final int notification_template_part_time = 0x7f090023;
     }
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0b0029;
+        public static final int status_bar_notification_info_overflow = 0x7f0b002c;
     }
     public static final class style {
         private style() {}
